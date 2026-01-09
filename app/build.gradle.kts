68d4c03f759fd81eaff40347d3f7d87e8b498546
miraitag <ruiztahuilan@gmail.com>
2026-01-09 16:10:55 -0600

    feature(VersionCatalog): Add coil to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    //coil
+    implementation(libs.coil.compose)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
